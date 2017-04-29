bc01ebe2f57dc53ffa8dc22a089dbe546ce2be68
manol <manol@DESKTOP-8C07824>
2017-04-29 14:50:28 +0200

    Ejercicios chulicos

---
+++ JavaApplication3/src/javaapplication3/Ejercicios1.java
@@ -0,0 +1,10 @@
+package javaapplication3;
+public class Ejercicios1 {
+    public static void main(String[] args) {      
+        int n2=30, suma=0, n1=10;
+        suma=n1+n2;
+        System.out.println("LA SUMA ES: "+ suma);
+        suma=suma+n1;
+        System.out.println(suma); 
+    }
+}
